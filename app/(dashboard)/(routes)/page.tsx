6c4cc55ca658bfd240fa5e0eb84d89283a3a1d5d
Ricardo Garcia Bahena <ricardogarciabahena@Ricardos-MacBook-Air.local>
2023-09-26 05:09:36 -0700

    finished auth

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,8 +1,9 @@
-
-const Home = () => {
+import { UserButton } from "@clerk/nextjs";
+ 
+export default function Home() {
   return (
-    <div>This is a protected page</div>
+    <div>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
   )
 }
-
-export default Home
